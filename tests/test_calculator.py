fd3c817c1391af2a2b301ed9658248358a918b04
arafat <arafat.hossen@alora.cloud>
2026-06-16 11:42:53 +0600

    test: fix broken add test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,7 @@ from calculator import add, subtract, divide, modulo, sqrt, power
 
 # --- add() ---
 def test_add_positive_numbers():
-    assert add(2, 3) == 99
+    assert add(2, 3) == 5
 
 
 def test_add_negative_numbers():
